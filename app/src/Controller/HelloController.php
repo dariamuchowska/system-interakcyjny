1cea5e44b443a6f47a964ba4df17230d2dbda8d4
Student <student@localhost>
2022-05-11 11:08:40 +0200

    commit

--- app/src/Controller/HelloController.php
+++ app/src/Controller/HelloController.php
@@ -2,13 +2,29 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController {
-    #[Route('/hello', name: 'hello_index', methods: 'GET')]
-    public function index(): Response
+#[Route('/hello')]
+class HelloController extends AbstractController
+{
+    /**
+     * http://localhost:8000/hello?name=John
+     * http://localhost:8000/hello/John
+     */
+    #[Route(
+        '/{name}',
+        name: 'hello_index',
+        requirements: ['name' => '[a-zA-Z0-9]+'],
+        defaults: ['name' => 'World'],
+        methods: 'GET'
+    )]
+    public function index(string $name): Response
     {
-        return new Response('Hello World!');
+        return $this->render(
+            'hello/index.html.twig',
+            ['name' => $name]
+        );
     }
 }
